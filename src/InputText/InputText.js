4c96fa63f49ba42149fb77148ae53ef18ca246d1
akshaypalshikar <aksmakesit@gmail.com>
2018-07-10 22:20:31 -0700

    validate component added

--- src/InputText/InputText.js
+++ src/InputText/InputText.js
@@ -1,4 +1,5 @@
 import  React  from 'react';
+import ValidateComponent from '../ValidateComponent/ValidateComponent'
 
 const inputText = (props) => {
     return(
@@ -6,6 +7,7 @@ const inputText = (props) => {
             <textarea onChange={props.changed} value={props.text}/>
             <br/>
             <label>Length</label> {props.length}
+            <ValidateComponent {...props}/>
         </div>
     );
 }
